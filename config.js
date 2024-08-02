056d745eaae20658e9739ecf0b5f5bd2a26afa58
Eboxsl <Eboxsl@hotmail.com>
2024-08-02 12:32:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,8 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 
 //═══════[Required Variables for Elite md]════════\\
+
+
 global.owner = process.env.OWNER_NUMBER || '94775686166'
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://Jithula:Jithula@cluster0.i9f4p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 global.port= process.env.PORT || 5000
@@ -16,7 +18,7 @@ global.website = 'http://www.ebox.free.nf/?i=1' //wa.me/+91000000000000
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/ad72b729bc880fa0d13f3.png'
 module.exports = {
   
-  sessionName:  process.env.SESSION_ID ||  "IZUKU;;; eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMlB3VnFYRnpGUUFHQUxFWFN5WFBLT05oaVlLWGxiejFueGthczRnUGNsQT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoidUIyRDhSVEpTVHp4bFFIcldNNk9TejAzUStPRk16dVhCVWhtRjRKSXEyYz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJlRHJsanc4aXNHaHh0QXp0ZTQ3ellobk9nYVZ5QWt0MnNqYjAzbDVWeWtZPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJWZUpQNEhpVktVb0RHV3VEZFNJYjJIQjMzL0k1MHNWUlBoSmVrZUhYSTBrPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Ik9IN2wzUzhNMFVZZlVGMmNZYzU4eW5kM0ViUzdYdm1LOXlYN2xwSzA2RUE9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlRSQ2UramlxZ3ZMWXJDQ3JHYWErTHEyMS9kTk44dWxKQlNtaVd6UmFyZzQ9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoib0dUY1Y3M3dWdDZDR29tZTQrTDVyZkZvK0tjeEZsalFHa29qb1FQSkdHWT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiUnJHVC9ieEN3YVFHR0dBUk9OaWJST3MrQWNNSTNocVB4UlVRVE1zcWxBRT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkpRMzJXdnh2a1Z6Tm9UTnltK3RUbSsxTlZPckNLamVid3dzNHFqTmNoSW9uWGFkUC9SSHNhY29KNVlOT3Rib3dUaDBHUTNPUlo1OUV3aUN5WEpUUGlnPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTAzLCJhZHZTZWNyZXRLZXkiOiIxcTBBWk5iMXp0NUxlb1RxU2ZmZFdUbWlKOUpOZ1JaampudGt2MUI0QTRNPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJyU0JpSHFaN1NKQ0JPOHBTalZWSDdnIiwicGhvbmVJZCI6IjFiODJkOWQ3LWQyNTItNDNjMi1hNDM0LTU2ODBiMjZiZWM0MiIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ6Z1hzRkYrRjk2QTFkTzhvWFg2cUxzckZDT289In0sInJlZ2lzdGVyZWQiOmZhbHNlLCJiYWNrdXBUb2tlbiI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InVqc0U1QnlHdlFFUy95NE5lRkptMFhSTTVuRT0ifSwicmVnaXN0cmF0aW9uIjp7fSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ05HbDNPWUJFT1RMbzdRR0dBTWdBQ2dBIiwiYWNjb3VudFNpZ25hdHVyZUtleSI6IlZyMytuRXRKTEUrNlJJYWZRdFZjdmN2TlBjR1NGUVRlcnhKMzBiZzBheVE9IiwiYWNjb3VudFNpZ25hdHVyZSI6IlhsVDFDV09Ndm8rV2tBbXo3czNuZGE5d2ZvcU41WVVWOUJlclVXQ3lWQ0FPbEFPUW51bVR6NUlGaEtlV3ljVVo3c05aVTRxOUpINVA5SllkQW42TUFnPT0iLCJkZXZpY2VTaWduYXR1cmUiOiJONHJCQ1VIaDB2TG82OHNpcXJUQ2FFREtXNkYwUFlzVkNPZVFLa3dqSGFHVHZXa1UybEdyRjVXTkY0b21CbkxDKzN6bWVGYW1JMnAvWVVoWEpxeFJnUT09In0sIm1lIjp7ImlkIjoiOTQ3NzU2ODYxNjY6NTVAcy53aGF0c2FwcC5uZXQifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiOTQ3NzU2ODYxNjY6NTVAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCVmE5L3B4TFNTeFB1a1NHbjBMVlhMM0x6VDNCa2hVRTNxOFNkOUc0TkdzayJ9fV0sInBsYXRmb3JtIjoiYW5kcm9pZCIsImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTcyMDI0Nzc4MywibXlBcHBTdGF0ZUtleUlkIjoiQUFBQUFGOVcifQ==",
+  sessionName:  process.env.SESSION_ID ||  "Pur sesin id here",
   botname:   process.env.BOT_NAME === undefined ? 'Elite BOT' : process.env.BOT_NAME,
   ownername: process.env.OWNER_NAME === undefined ? 'Nethindu ' : process.env.OWNER_NAME,
   author:  process.env.PACK_INFO === undefined ? 'No name' : process.env.PACK_INFO.split(";")[0],
@@ -27,8 +29,8 @@ module.exports = {
   alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
   typingprensence:  process.env.TYPING === undefined ? false : process.env.TYPING,
   autosendstatus:  process.env.STATUS_SENDER === undefined ? false : process.env.STATUS_SENDER,
-anticall:  process.env.ANTI_CALL === undefined ? false : process.env.ANTI_CALL,
-recordingpresence:  process.env.RECORDING === undefined ? false : process.env.RECORDING,	
+  anticall:  process.env.ANTI_CALL === undefined ? false : process.env.ANTI_CALL,
+  recordingpresence:  process.env.RECORDING === undefined ? false : process.env.RECORDING,	
   antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '971' : process.env.FAKE_COUNTRY_CODE,
   readmessage:  process.env.READ_MESSAGE === undefined ? false : process.env.READ_MESSAGE,
   auto_status_saver: process.env.AUTO_STATUS_SAVER === undefined ? false : process.env.AUTO_STATUS_SAVER,
