30dd87c47db0c35b51ef3b002564d928127ba559
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2024-08-07 12:38:58 +0530

    Update logo.js

--- commands/logo.js
+++ commands/logo.js
@@ -3,7 +3,7 @@
  const Config = require('../config')
  const prefix = Config.prefix
  const maker = require('mumaker')
- let cap = ` © 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 ᳆⃞⃚🇱🇰ʀͥᴇᷧᴀͫʟ⃟   𝙴𝙻𝙸𝚃𝙴-𝚖𝚍`
+ let cap = ``
      //---------------------------------------------------------------------------
  cmd({ pattern: "deepsea", category: "textpro", desc: "Some text to image feature with various styles." }, async(Void, citel, text) => {
          if (!text) return citel.reply('_Need text._')
